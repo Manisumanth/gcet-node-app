90583572d8bba1249bb9ba1ca11ddafbdfb4af48
Manisumanth <manisumanthk@gmail.com>
2025-06-05 09:43:41 +0530

    updated

--- index1.js
+++ index1.js
@@ -1,48 +1,52 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI
+
 app.use(cors()); 
 app.use(express.json());
 
-app.listen(8080, () => {
-    mongoose.connect("mongodb://localhost:27017/gcet");
-  console.log("Server Started on port 8080");
-});
 
-const userSchema = mongoose.Schema({
-    name: {type: String},
-    email : {type : String},
-    pass : {type : String}
-});
 
-const user = mongoose.model("User", userSchema);
+app.use("/orders", orderRouter); 
 
-app.post("/register", async(req, res)=>{
-    const {name, email, pass} = req.body
-    const result = await user.insertOne({name, email, pass});
-    return res.json(result);
-})
+app.use("/users", userRouter);
+
+app.use("/products", productRouter);
+
 
 app.get("/", (req, res) => {
-  return res.send("Hello! Good morning xD");
+  return res.send("Hello! Good morning :)");
 });
 
 app.get("/weather", (req, res) => {
-  return res.send("Hola Amigos! Today's weather is 33"); 
+  return res.send("Heyy!! Today's weather is 28"); 
 });
 
 app.get("/name", (req, res) => {
-  return res.send("Hello Sumanth!"); 
+  return res.send("Hello Sumanth!!"); 
 });
 
-app.get("/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 999 },
-    { id: 2, name: "Phone", price: 499 },
-    { id: 3, name: "Headphones", price: 199 }
-  ];
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
   
-  return res.json(products);
-});
---
+++ models/productModel.js
@@ -0,0 +1,11 @@
+
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: { type: String },
+  description: { type: String },
+  imgUrl: { type: String },
+  price: { type: Number },
+});
+
+export default mongoose.model("Product", productSchema);
